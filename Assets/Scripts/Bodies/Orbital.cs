3e1075e274e071df934b4d22e1b3e854d9f5b5a5
qfeys <qfeys@skynet.be>
2016-10-05 19:16:17 +0200

    Set up of orbitals

---
+++ Assets/Scripts/Bodies/Orbital.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Scripts.Bodies
+{
+    abstract class Orbital
+    {
+        Orbital Parent;
+        List<Orbital> Childeren;
+        ulong mass;
+        OrbitalElements elements;
+    }
+
+    /// <summary>
+    /// orbital elements. All angles in radian
+    /// </summary>
+    struct OrbitalElements
+    {
+        double LAN; // longitude of ascending node
+        double i;   // inclination
+        double AOP; // Argument of periapsis
+        double MAaE;// Mean anomaly at epoch
+        ulong SMA;  // Semi-major axis
+        double e;   // exentricity
+    }
+}
---
+++ Assets/Scripts/Rendering/God.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Assets.Scripts.Rendering
+{
+    public class God : MonoBehaviour
+    {
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
