413151e30b01ee268baee87bbb2a80a5e36c0928
qfeys <qfeys@skynet.be>
2016-10-05 22:59:57 +0200

    add StarSystem

---
+++ Assets/Scripts/Bodies/StarSystem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Scripts.Bodies
+{
+    class StarSystem : Orbital
+    {
+        public StarSystem(Orbital parent, long mass, OrbitalElements elements) : base(parent, 0, elements)
+        {
+        }
+
+        public override void Generate(System.UInt64 mass, Random rand)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
