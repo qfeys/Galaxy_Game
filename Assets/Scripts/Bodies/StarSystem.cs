08c666ceab946482be256e41e20fac2218f3f62b
qfeys <qfeys@skynet.be>
2016-10-10 17:24:11 +0200

    Finalising Giant creation

--- Assets/Scripts/Bodies/StarSystem.cs
+++ Assets/Scripts/Bodies/StarSystem.cs
@@ -48,43 +48,49 @@ namespace Assets.Scripts.Bodies
             //(4*R*R*e^(-(2*R)/m))/m^3                      Probability function
             // P = 1- exp(-2*R/m)*(m^2+2*m*R+2*R^2)/m^2     cumulative probability function, maps to [0,1]
             // ((log(10,x*10+1)*log(11,10)+1)/2             formulla most likly radius based on mass
-            double massRation = gMass / Giant.JupiterMass;
-            ulong m = (ulong)((Math.Log(10 * massRation + 1) / Math.Log(11) + 1) / 2 * Giant.JupiterSMA);    // mean, the most likely radius
-            UnityEngine.Debug.Log("ratio: " + massRation + " m: " + m);
-            ulong R = m;                                                        // Radius, an estimate of the sma
+            double massRatio = gMass / Giant.JupiterMass;
+            ulong m = (ulong)((Math.Log(10 * massRatio + 1) / Math.Log(11) + 1) / 2 * Giant.JupiterSMA);    // mean, the most likely radius
 
-            Func<ulong, double> P = r => (4 * r * r * Math.Exp(-(2.0 * r) / m)) / (m * m * m);
-            Func<ulong, ulong, double> Q = (r, k) => Math.Exp(-1.0 / Math.Pow(r - k, 2));
-
-            List<Tuple<double, ulong>> C = NormRiemannSum(P,m);
+            Func<ulong, double> P = r => (4.0 * r * r * Math.Exp(-2.0 * r / m)) / (1.0 * m * m * m);
+            Func<ulong, ulong, double> Q = (r, k) => Math.Exp(-1.0 * k * k / 10 / Math.Pow(r - k, 2));
+            List<Func<ulong, double>> Px = new List<Func<ulong, double>>();
+            Px.Add(P);
+            foreach (var g in Childeren.FindAll(c => c.GetType() == typeof(Giant)))
+            {
+                Px.Add(r =>Q(r, g.Elements.SMA));
+            }
+            List<Tuple<double, ulong>> C = NormRiemannSum(Px,m);
 
             // Find the last tuple where (rand-item1 > 0). This is the left border
             int lb = C.FindLastIndex(t => rand - t.Item1 > 0);
             // Preform and return an linear interpolation on C
-            return C[lb].Item2 + (ulong)((rand - C[lb].Item1) / (C[lb + 1].Item1 - C[lb].Item1) * (C[lb + 1].Item2 - C[lb].Item2));
+            ulong R = C[lb].Item2 + (ulong)((rand - C[lb].Item1) / (C[lb + 1].Item1 - C[lb].Item1) * (C[lb + 1].Item2 - C[lb].Item2));
+
+            UnityEngine.Debug.Log("massRatio: " + massRatio + " Mean location: " + m.ToString("e2") + " Actual location: " + R.ToString("e2") + " Rand: " + rand);
+            return R;
         }
 
         /// <summary>
         /// Projects the riemann sum of function p(x) to (int(p,0,t),t) for t = 0 to 6*m
         /// Then it normilizes this so the maximum sum is 1
         /// </summary>
-        private List<Tuple<double, ulong>> NormRiemannSum(Func<ulong, double> p, ulong m)
+        private List<Tuple<double, ulong>> NormRiemannSum(List<Func<ulong, double>> p, ulong m)
         {
             List<Tuple<double, ulong>> ret = new List<Tuple<double, ulong>>();
             double s = 0;
             for (ulong i = 0; i < m * 6; i += m / 20)    // 120 iterations
             {
-                s += p(i) * m / 20;         // Right riemann sum
+                double term = 1;
+                foreach (var q in p)
+                {
+                    term *= q(i);
+                }
+                s += term * m / 20;         // Right riemann sum
                 ret.Add(new Tuple<double, ulong>(s, i));
             }
             // Normilize all points so end point is 1
             ret.ForEach(t => t.Item1 *= 1 / s);
             return ret;
-        }
-
-        private Dictionary<double, ulong> NormAndInv(Dictionary<ulong, double> c)
-        {
-            throw new NotImplementedException();
         }
 
         public const ulong AU = 149597870700;
--- Assets/Scripts/Rendering/God.cs
+++ Assets/Scripts/Rendering/God.cs
@@ -19,7 +19,7 @@ namespace Assets.Scripts.Rendering
             log = new List<string>();
             Time = 0;
 
-            Bodies.Core.Create(1, 22);
+            Bodies.Core.Create(1, 20);
             log.ForEach(l => Debug.Log(l));
             DisplayManager.TheOne.DisplaySystem((Bodies.StarSystem)Bodies.Core.instance.Childeren[0]);
         }
