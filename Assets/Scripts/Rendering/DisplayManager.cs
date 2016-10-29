9e6ff574a31c3b1380ef4165ece13cccd30dfb08
qfeys <qfeys@skynet.be>
2016-10-29 13:00:33 +0200

    seperated system rendering to seperate class

--- Assets/Scripts/Rendering/DisplayManager.cs
+++ Assets/Scripts/Rendering/DisplayManager.cs
@@ -9,23 +9,18 @@ namespace Assets.Scripts.Rendering
     public class DisplayManager : MonoBehaviour
     {
         static public DisplayManager TheOne;
-        Dictionary<GameObject, Bodies.Orbital> DisplayedBodies;
-        Dictionary<GameObject, LineRenderer> DisplayedOrbits;
+        SystemRenderer systemrenderer;
         public GameObject protoStar;
         public GameObject protoGiant;
         public GameObject protoRock;
-        Dictionary<Type, Func<GameObject>> protoInstantiation;
         public float zoom = 12; // log scale - high values are zoomed in
 
         public void Awake()
         {
             if (TheOne != null) throw new Exception("A second display manager is created");
             TheOne = this;
-            protoInstantiation = new Dictionary<Type, Func<GameObject>> {
-                { typeof(Star), () =>  Instantiate(protoStar) },
-                { typeof(Giant), () => Instantiate(protoGiant) },
-                { typeof(Rock), () => Instantiate(protoRock) }
-            };
+            systemrenderer = gameObject.AddComponent<SystemRenderer>();
+            systemrenderer.InstantiatePrototypes(protoStar, protoGiant, protoRock);
         }
 
         // Use this for initialization
@@ -41,38 +36,13 @@ namespace Assets.Scripts.Rendering
 
         internal void DisplaySystem(Bodies.StarSystem syst)
         {
-            DisplayedBodies = new Dictionary<GameObject, Bodies.Orbital>();
-            DisplayedOrbits = new Dictionary<GameObject, LineRenderer>();
-            GameObject star = null;
-            syst.Childeren.ForEach(b =>
-            {
-                GameObject go = protoInstantiation[b.GetType()]();
-                if (star == null && b.GetType() == typeof(Bodies.Star)) star = go;
-                Bodies.VectorS posS = b.Elements.GetPositionSphere(God.Time);
-                float scale = Mathf.Pow(10, -zoom);
-                Vector3 v = (Vector3)posS * scale;
-                go.transform.position = v;
-                DisplayedBodies.Add(go, b);
-                if (b.GetType() != typeof(Bodies.Star))
-                {
-                    go.transform.SetParent(star.transform);
-
-                    GameObject orb = new GameObject("Orbit of " + b);
-                    orb.transform.SetParent(star.transform);
-                    LineRenderer lr = orb.AddComponent<LineRenderer>();
-                    lr.SetVertexCount(40);
-                    lr.SetWidth(0.1f, 0.2f);
-                    lr.SetPositions(FindPointsOnOrbit(b.Elements, 40));
-                    DisplayedOrbits.Add(go, lr);
-                }
-            });
+            systemrenderer.SetSystem(syst);
+            systemrenderer.Render();
         }
 
-        private Vector3[] FindPointsOnOrbit(OrbitalElements elements, int number)
+        internal void RerenderSystem()
         {
-            //elements.FindPointsOnOrbit(20).ToList().ForEach(vs => Debug.Log(vs));
-            return elements.FindPointsOnOrbit(number).Select(vs => (Vector3)vs * Mathf.Pow(10, -zoom)).ToArray();
-            
+            systemrenderer.Render();
         }
     }
 }
---
+++ Assets/Scripts/Rendering/SystemRenderer.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Assets.Scripts.Bodies;
+using UnityEngine;
+
+namespace Assets.Scripts.Rendering
+{
+    class SystemRenderer : MonoBehaviour
+    {
+
+        Dictionary<GameObject, Bodies.Orbital> DisplayedBodies;
+        Dictionary<GameObject, LineRenderer> DisplayedOrbits;   // The gameobject is the orbital, not the line
+        Dictionary<Type, GameObject> prototypes;
+
+        public float zoom = 10.8f; // log scale - high values are zoomed in
+
+        public void Awake()
+        {
+
+        }
+
+        public void InstantiatePrototypes(GameObject star, GameObject giant, GameObject rock)
+        {
+            prototypes = new Dictionary<Type, GameObject> {
+                { typeof(Bodies.Star), star },
+                { typeof(Bodies.Giant), giant },
+                { typeof(Bodies.Rock), rock }
+            };
+        }
+
+        internal void SetSystem(StarSystem syst)
+        {
+            DisplayedBodies = new Dictionary<GameObject, Bodies.Orbital>();
+            DisplayedOrbits = new Dictionary<GameObject, LineRenderer>();
+            GameObject star = null;
+            syst.Childeren.ForEach(b =>
+            {
+                GameObject go = Instantiate(prototypes[b.GetType()]);
+                go.name = b.ToString();
+                if (star == null && b.GetType() == typeof(Bodies.Star)) star = go;
+                DisplayedBodies.Add(go, b);
+                if (b.GetType() != typeof(Bodies.Star))
+                {
+                    go.transform.SetParent(star.transform);
+
+                    GameObject orb = new GameObject("Orbit of " + b);
+                    orb.transform.SetParent(star.transform);
+                    LineRenderer lr = orb.AddComponent<LineRenderer>();
+                    lr.SetVertexCount(40);
+                    lr.SetWidth(0.1f, 0.2f);
+                    DisplayedOrbits.Add(go, lr);
+                }
+            });
+        }
+
+        public void Render()
+        {
+            GameObject star = null;
+            foreach(var b in DisplayedBodies)
+            {
+                if (star == null && b.Value.GetType() == typeof(Bodies.Star)) star = b.Key;
+                VectorS posS = b.Value.Elements.GetPositionSphere(God.Time);
+                float scale = Mathf.Pow(10, -zoom);
+                Vector3 v = (Vector3)posS * scale;
+                b.Key.transform.position = v;
+                if (b.GetType() != typeof(Bodies.Star))
+                {
+                    DisplayedOrbits[b.Key].SetPositions(FindPointsOnOrbit(b.Value.Elements, 40));
+                }
+            });
+        }
+
+        private Vector3[] FindPointsOnOrbit(OrbitalElements elements, int number)
+        {
+            //elements.FindPointsOnOrbit(20).ToList().ForEach(vs => Debug.Log(vs));
+            return elements.FindPointsOnOrbit(number).Select(vs => (Vector3)vs * Mathf.Pow(10, -zoom)).ToArray();
+
+        }
+    }
+}
