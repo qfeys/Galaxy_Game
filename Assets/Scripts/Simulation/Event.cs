710a42f58116cd6c1f49f5398b7140148697feac
qfeys <qfeys@skynet.be>
2017-03-09 15:58:50 +0100

    First version of event system (wip)

---
+++ Assets/Scripts/Simulation/Event.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Scripts.Simulation
+{
+    class Event : IComparable<Event>
+    {
+
+        public readonly DateTime date;
+        public readonly Action effect;
+        public enum Interrupt { none, soft, hard }
+
+        public int CompareTo(Event other)
+        {
+            return date.CompareTo(other.date);
+        }
+    }
+}
---
+++ Assets/Scripts/Simulation/EventSchedule.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using C5;
+
+namespace Assets.Scripts.Simulation
+{
+    static class EventSchedule
+    {
+        static TreeSet<Event> events;
+
+        public static DateTime nextEvent { get { return events.FindMin().date; } }
+
+        static void Init()
+        {
+            events = new TreeSet<Event>();
+        }
+
+        static void Add(Event newEvent)
+        {
+            events.Add(newEvent);
+        }
+    }
+}
