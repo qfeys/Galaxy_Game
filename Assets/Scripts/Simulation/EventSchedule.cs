3c08fbe0729b91e672a691a4c291ac4bbc2b3b17
qfeys <qfeys@skynet.be>
2017-03-13 11:45:24 +0100

    Removed dependancy on C5 with custom 'sorted List'

--- Assets/Scripts/Simulation/EventSchedule.cs
+++ Assets/Scripts/Simulation/EventSchedule.cs
@@ -2,15 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using C5;
 
 namespace Assets.Scripts.Simulation
 {
     static class EventSchedule
     {
-        static TreeSet<Event> events = new TreeSet<Event>();
+        static SortedList<Event> events = new SortedList<Event>();
 
-        public static DateTime nextEvent { get { return events.FindMin().date; } }
+        public static DateTime nextEvent { get { return events.FindFirst().date; } }
         
 
         public static void Add(Event newEvent)
--- Assets/Scripts/Utils.cs
+++ Assets/Scripts/Utils.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -139,5 +140,85 @@ namespace Assets.Scripts
         {
             return new TimeSpan((long)(mtth.Ticks * -Math.Log(rand.NextDouble(), 2)));
         }
+    }
+
+    public class SortedList<T> : ICollection<T>
+    {
+        private readonly List<T> collection = new List<T>();
+        // TODO: initializable:
+        private readonly IComparer<T> comparer = Comparer<T>.Default;
+
+        public void Add(T item)
+        {
+            if (Count == 0)
+            {
+                collection.Add(item);
+                return;
+            }
+            int minimum = 0;
+            int maximum = collection.Count - 1;
+
+            while (minimum <= maximum)
+            {
+                int midPoint = (minimum + maximum) / 2;
+                int comparison = comparer.Compare(collection[midPoint], item);
+                if (comparison == 0)
+                {
+                    return; // already in the list, do nothing
+                }
+                if (comparison < 0)
+                {
+                    minimum = midPoint + 1;
+                }
+                else
+                {
+                    maximum = midPoint - 1;
+                }
+            }
+            collection.Insert(minimum, item);
+        }
+
+        public bool Contains(T item)
+        {
+            // TODO: potential optimization
+            return collection.Contains(item);
+        }
+
+        public bool Remove(T item)
+        {
+            // TODO: potential optimization
+            return collection.Remove(item);
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return collection.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public void Clear()
+        {
+            collection.Clear();
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            collection.CopyTo(array, arrayIndex);
+        }
+
+        internal T FindFirst()
+        {
+            if (collection.Count != 0)
+                return collection[0];
+            else
+                throw new NullReferenceException("This sorted list is empty");
+        }
+
+        public int Count { get { return collection.Count; } }
+        public bool IsReadOnly { get { return false; } }
     }
 }
