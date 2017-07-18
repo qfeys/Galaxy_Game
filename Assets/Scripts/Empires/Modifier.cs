9544b4081ad4b95440831b7774309da8531cb019
qfeys <qfeys@skynet.be>
2017-07-18 12:44:21 +0200

    Fixing the modefier class, because I had already made one

--- Assets/Scripts/Empires/Modifier.cs
+++ Assets/Scripts/Empires/Modifier.cs
@@ -7,6 +7,23 @@ namespace Assets.Scripts.Empires
 {
     class Modifier
     {
+        public readonly Name name;
+        public readonly double value;
+
+        public Modifier(Name name, double value)
+        {
+            this.name = name;
+            this.value = value;
+        }
+
+        public Modifier(string name, double value)
+        {
+            this.name = (Name)Enum.Parse(typeof(Name), name);
+            this.value = value;
+        }
+
+        public enum Name { add_industry_capacity }
+
         internal enum PopulationModifier { localBuildTime, wealth, }
         internal enum GlobalModifier { globalBuildTime }
         internal enum AssetModifier { upkeepReduction}
