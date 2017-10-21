de94333097c3fbdb256008e1bfaf6f5b1794f0ab
qfeys <qfeys@skynet.be>
2017-10-21 13:12:00 +0200

    bug equals resources

--- Assets/Scripts/Empires/Production/Stockpile.cs
+++ Assets/Scripts/Empires/Production/Stockpile.cs
@@ -60,19 +60,9 @@ namespace Assets.Scripts.Empires.Production
             // override object.Equals
             public override bool Equals(object obj)
             {
-                //       
-                // See the full list of guidelines at
-                //   http://go.microsoft.com/fwlink/?LinkID=85237  
-                // and also the guidance for operator== at
-                //   http://go.microsoft.com/fwlink/?LinkId=85238
-                //
-
                 if (obj == null || GetType() != obj.GetType())
-                {
                     return false;
-                }
-
-                return type.Equals(obj);
+                return type.Equals(((ResourceType)obj).type);
             }
 
             // override object.GetHashCode
